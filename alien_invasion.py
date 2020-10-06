2acb31e89c1774d7665dc3beab6e01423e236ee4
Nazim Rizvic <nazim.rizvic@me.com>
2020-10-07 09:59:28 +1100

    Added ship class and images folder for game elements.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,11 +2,18 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 def run_game():
 	#initialize game and create a screen object.
 	pygame.init()
-	screen = pygame.display.set_mode((1200, 800))
+	ai_settings = Settings()
+	screen = pygame.display.set_mode(
+		(ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
+
+	#set the background colour.
+	bg_colour = (230, 230 , 230)
 
 	#Start the main loop for the game.
 	while True:
@@ -15,6 +22,8 @@ def run_game():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
+		#Redraw the screen during each pass through loop
+		screen.fill(ai_settings.bg_colour)
 
 		#make the most recently drawn screen visible.
 		pygame.display.flip()
